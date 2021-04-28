8bad864dd4bac794fbbc7d5f3b76cadfe69d7d4c
JayIssuree <jay_issuree@hotmail.co.uk>
2021-04-28 13:43:06 +0100

    set up jquery interface and html

--- jasmine/spec/FeatureSpec.js
+++ jasmine/spec/FeatureSpec.js
@@ -32,5 +32,18 @@ describe("Features", function(){
         })
 
     })
+
+    describe("getCumulativeScore", function(){
+
+        it("returns the cumulative score of the frames", function(){
+            game.roll(10)
+            game.roll(4)
+            game.roll(6)
+            game.roll(8)
+            game.roll(1)
+            expect(game.getCumulativeScores()).toEqual([20, 38, 47])
+        })
+
+    })
     
 })
