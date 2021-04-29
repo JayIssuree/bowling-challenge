41a0d38a0938ce382106ef462f6de1fd8df080dd
JayIssuree <jay_issuree@hotmail.co.uk>
2021-04-29 12:34:45 +0100

    added 1 - 10 pin buttons

--- interface.js
+++ interface.js
@@ -2,11 +2,13 @@ $(document).ready(function () {
 
     var game = new Game();
     setUpTable()
+    setUpPins()
 
     function createTable(){
       var tableDiv = document.createElement('div')
       var table = document.createElement('table')
       var frameNumber = document.createElement('tr')
+      tableDiv.className = "scorecardDiv"
       table.className = "scorecard"
       frameNumber.className = "frameNumber"
       table.appendChild(frameNumber)
@@ -18,15 +20,35 @@ $(document).ready(function () {
       var frames = game.getFrames()
       for(var i = 0; i < frames.length; i++) {
         var frameNumber = document.createElement('td')
-        frameNumber.innerHTML = (`Frame ${i + 1}`)
+        frameNumber.setAttribute('colspan', 2)
+        frameNumber.innerHTML = `Frame ${i + 1}`
         $(frameNumber).appendTo(".frameNumber")
       }
     }
-    
+
     function setUpTable(){
       createTable()
       insertFrameNumbersIntoTable()
     }
+
+    function createPins(){
+      rollsDiv = document.createElement('div')
+      rollsDiv.className = 'pinsToRoll'
+      for (var i = 1; i <= 10; i++){
+        pinButton = document.createElement('button')
+        pinButton.innerHTML = i
+        pinButton.className = `pin${i}`
+        rollsDiv.appendChild(pinButton)
+      }
+      $(rollsDiv).appendTo("body")
+    }
+
+    function setUpPins(){
+      createPins()
+    }
+
+    // function updateBasicRolls(){
+    // }
   
   });
   
