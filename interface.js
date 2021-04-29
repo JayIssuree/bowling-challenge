de53a6f4b235ae9b518f4faed15c59b3faf9ae2e
JayIssuree <jay_issuree@hotmail.co.uk>
2021-04-29 12:01:04 +0100

    created scorecard table and displays frame numbers

--- interface.js
+++ interface.js
@@ -1,8 +1,32 @@
 $(document).ready(function () {
-    var game = new Game();
-    
 
-  
+    var game = new Game();
+    setUpTable()
+
+    function createTable(){
+      var tableDiv = document.createElement('div')
+      var table = document.createElement('table')
+      var frameNumber = document.createElement('tr')
+      table.className = "scorecard"
+      frameNumber.className = "frameNumber"
+      table.appendChild(frameNumber)
+      tableDiv.appendChild(table)
+      $(tableDiv).appendTo("body")
+    }
+
+    function insertFrameNumbersIntoTable(){
+      var frames = game.getFrames()
+      for(var i = 0; i < frames.length; i++) {
+        var frameNumber = document.createElement('td')
+        frameNumber.innerHTML = (`Frame ${i + 1}`)
+        $(frameNumber).appendTo(".frameNumber")
+      }
+    }
+    
+    function setUpTable(){
+      createTable()
+      insertFrameNumbersIntoTable()
+    }
   
   });
   
